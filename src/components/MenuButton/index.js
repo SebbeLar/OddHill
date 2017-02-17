e65bb8d15ad7b9df446084aca52e2a425a202b24
Sebastian Larsson <sebberlarsson@hotmail.com>
2017-02-17 10:53:46 +0100

    Added menu button in details container

---
+++ src/components/MenuButton/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MenuButton = () => {
+  return (
+    <button className="details-menuButton">
+      <img src="/static/svg/menu.svg" className="details-menuButton-background" />
+    </button>
+  );
+};
+
+export default MenuButton;
--- src/containers/DetailsContainer/index.js
+++ src/containers/DetailsContainer/index.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import MenuButton from '../../components/MenuButton';
 
 class Details extends Component {
   render() {
     return (
       <div className="details-container">
-        This is details container
+        <img src="/static/svg/bg.png"
+             srcSet="/static/svg/bg@2x.png, /static/svg/bg@3x.png"
+             className="details-background" />
+        <MenuButton />
       </div>
     );
   }

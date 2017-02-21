4cd6c866601544233b989e0f21355b1db2d94087
Sebastian Larsson <sebberlarsson@hotmail.com>
2017-02-21 09:12:07 +0100

    Added README and removed old commented out code

---
+++ README.md
@@ -0,0 +1,7 @@
+### OddHill Worksample ###
+
+###### Instructions ######
+
+1. `git clone https://github.com/SebbeLar/OddHill.git`
+2. `npm install`
+3. `npm run dev`
--- src/reducers/stateReducer.js
+++ src/reducers/stateReducer.js
@@ -6,23 +6,31 @@ export default function invoices(state = initialState, action) {
 
   case TOGGLE_FILTER:
     const newState = state.slice();
-      if(action.filter !== 'balance') {
-        newState.sort((a, b) => {
-          if(a[action.filter] < b[action.filter]) {
-            return -1;
-          }
-          if(a[action.filter] > b[action.filter]) {
-            return 1;
-          }
-          return 0;
-        });
-      } else {
-        newState.sort((a, b) => {
-          console.log('Hello');
-          return parseFloat(a[action.filter].substring(1)) - parseFloat(b[action.filter].substring(1));
-        });
-      }
+    if(action.filter === 'currency') {
+      return newState;
+    }
+    if(action.filter !== 'balance') {
+      newState.sort((a, b) => {
+        if(a[action.filter] < b[action.filter]) {
+          return -1;
+        }
+        if(a[action.filter] > b[action.filter]) {
+          return 1;
+        }
+        return 0;
+      });
+    } else {
+      newState.sort((a, b) => {
+        const invoiceA = parseFloat(a[action.filter].substring(1).replace(',', ''));
+        const invoiceB = parseFloat(b[action.filter].substring(1).replace(',', ''));
+        return invoiceA - invoiceB;
+      });
+    }
+    if(state[0][action.filter] === newState[0][action.filter]) {
+      return newState.reverse();
+    }
     return newState;
+
   default:
     return state;
   }
